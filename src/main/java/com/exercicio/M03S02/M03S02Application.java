f5fb69d5de5dfc32225f0fbdbda78011de47dbda
David Alves Dutra <dadutra@hotmail.com>
2024-09-11 14:33:54 -0300

    First commit

---
+++ src/main/java/com/exercicio/M03S02/M03S02Application.java
@@ -0,0 +1,13 @@
+package com.exercicio.M03S02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M03S02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M03S02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercicio/M03S02/M03S02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.M03S02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M03S02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
